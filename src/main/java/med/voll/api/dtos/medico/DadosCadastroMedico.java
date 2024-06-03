cf53a0a807dc299fe1774548907679b72a103636
Igorpereirag <igorpereira1501@gmail.com>
2024-06-03 13:31:41 -0300

    feat: mensagens de erro personalizadas

--- src/main/java/med/voll/api/dtos/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/dtos/medico/DadosCadastroMedico.java
@@ -9,19 +9,22 @@ import med.voll.api.dtos.endereco.DadosEndereco;
 import med.voll.api.enums.Especialidade;
 
 public record DadosCadastroMedico(
-        @NotBlank
+        @NotBlank(message = "Nome é obrigatório")
         String nome,
-        @NotBlank
-        @Email
+    
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Formato do email é inválido")
         String email,
-
-        @NotBlank
+    
+        @NotBlank(message = "Telefone é obrigatório")
         String telefone,
-        @NotBlank
-        @Pattern(regexp = "\\d{4,6}")
+    
+        @NotBlank(message = "CRM é obrigatório")
+        @Pattern(regexp = "\\d{4,6}", message = "Formato do CRM é inválido")
         String crm,
-        @NotNull
+    
+        @NotNull(message = "Especialidade é obrigatória")
         Especialidade especialidade,
-
-        @NotNull @Valid DadosEndereco endereco) {
-}
+    
+        @NotNull(message = "Dados do endereço são obrigatórios")
+        @Valid DadosEndereco endereco) {}
