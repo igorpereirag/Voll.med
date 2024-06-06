b3c1e1cc99cb0c4cb75a28be53315355ff26149d
Igorpereirag <igorpereira1501@gmail.com>
2024-06-06 20:09:57 -0300

    user detail service

---
+++ src/main/java/med/voll/api/infra/security/AutenticacaoService.java
@@ -0,0 +1,21 @@
+package med.voll.api.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import med.voll.api.repository.UsuarioRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
