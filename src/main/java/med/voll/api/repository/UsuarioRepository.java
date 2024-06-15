6e49191a210f852de802ca2ef4e9fc2310c3bfea
Igorpereirag <igorpereira1501@gmail.com>
2024-06-14 23:42:14 -0300

    removendo modulos não utlizados

--- src/main/java/med/voll/api/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package med.voll.api.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import med.voll.api.domain.usuario.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    UserDetails findByLogin(Object subject);
-}
